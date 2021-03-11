0c30e8a890821a692889d99584c2997b34ca1923
Aidar Sadykov <aidarsadykov94@gmail.com>
2021-03-11 14:03:42 +0100

    Create macro components: Game, Map, Tile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Game />
   );
+}
+
+class Game extends React.Component{
+  render(){
+    return (
+      <Map />
+    );
+  }
+}
+
+class Map extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      worldSizeX: 12,
+      worldSizeY: 8,
+    };  
+  }
+
+  render() {
+    var tiles = [];
+    var id = 0;
+    for (var i=0; i<this.state.worldSizeY; i++) {      
+      for (var j=0; j<this.state.worldSizeY; j++) {
+        tiles.push(<Tile key={id} />);
+        id += 1;        
+      }
+      tiles.push(<br key={id} />)
+      id += 1; 
+    }
+    return(tiles);
+  }
+}
+
+class Tile extends React.Component{
+  render(){
+    return(
+      <button className="tile">X</button>
+    );
+  }
 }
 
 export default App;
