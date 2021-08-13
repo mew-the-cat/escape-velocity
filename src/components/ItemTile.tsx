19b5ac0e9fa9c7657e92e376eca3f21154d13eb5
Aidar Sadykov <aidarsadykov94@gmail.com>
2021-08-13 15:34:53 +0200

    Restyle UI

--- src/components/ItemTile.tsx
+++ src/components/ItemTile.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const ItemTile = styled.input`
-  width: 150px;
+  width: 90%;
   height: 40px;
   margin: 3px 3px 3px 3px;
-  background-color: rgb(225, 137, 155);
+  background-color: rgb(148, 148, 184);
+  border-radius: 5px;
+  border: 2px solid rgb(209, 209, 224);
+  color: white;
 `;
