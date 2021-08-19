f80635e67b2ec9ff1a3b01ea2dd7d9c5a7b77d87
Aidar Sadykov <aidarsadykov94@gmail.com>
2021-08-19 12:54:07 +0200

    Continue conversion to function components

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -4,19 +4,17 @@ interface ActionBarProps {
   onClick: React.MouseEventHandler;
 }
 
-export class ActionBar extends React.Component<ActionBarProps> {
-  render() {
-    return (
-      <div>
-        <b>Actions</b>
-        <br />
-        <input
-          className="action"
-          type="button"
-          value="Search"
-          onClick={this.props.onClick}
-        />
-      </div>
-    );
-  }
-}
+export const ActionBar: React.FC<ActionBarProps> = ({ onClick }) => {
+  return (
+    <div>
+      <b>Actions</b>
+      <br />
+      <input
+        className="action"
+        type="button"
+        value="Search"
+        onClick={onClick}
+      />
+    </div>
+  );
+};
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,8 +2,8 @@ import "../styles/App.css";
 import Game from "./Game";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const App = () => {
   return <Game />;
-}
+};
 
 export default App;
