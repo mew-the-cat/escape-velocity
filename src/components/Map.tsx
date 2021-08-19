785cb5079d6b63518a2d8bbb4f3ae4693f4e0c7b
Aidar Sadykov <aidarsadykov94@gmail.com>
2021-08-19 17:23:57 +0200

    Finalize conversion to function components

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -7,27 +7,26 @@ interface MapProps {
   onClick: (col: number, row: number) => void;
 }
 
-export class Map extends React.Component<MapProps> {
-  render() {
-    const Cells = [];
-    for (let row = 0; row < this.props.tiles.length; row++) {
-      for (let col = 0; col < this.props.tiles[row].length; col++) {
-        const handleClickTileBound = this.props.onClick.bind(this, col, row);
-        Cells.push(
-          <CellTile
-            key={"Cell" + col + ":" + row}
-            tiletype={this.props.tiles[col][row].type}
-            characters={this.props.tiles[col][row].characters}
-            onClick={handleClickTileBound}
-          >
-            {this.props.tiles[col][row].characters.length !== 0
-              ? "X"
-              : "(" + col + "," + row + ")"}
-          </CellTile>
-        );
-      }
-      Cells.push(<br key={"LineBreak" + row} />);
+export const Map: React.FC<MapProps> = ({ tiles, onClick }) => {
+  const Cells = [];
+
+  for (let row = 0; row < tiles.length; row++) {
+    for (let col = 0; col < tiles[row].length; col++) {
+      const handleClickTileBound = onClick.bind(this, col, row);
+      Cells.push(
+        <CellTile
+          key={"Cell" + col + ":" + row}
+          tiletype={tiles[col][row].type}
+          characters={tiles[col][row].characters}
+          onClick={handleClickTileBound}
+        >
+          {tiles[col][row].characters.length !== 0
+            ? "X"
+            : "(" + col + "," + row + ")"}
+        </CellTile>
+      );
     }
-    return Cells;
+    Cells.push(<br key={"LineBreak" + row} />);
   }
-}
+  return <>{Cells}</>;
+};
