61d3dbfda26e0ff0dec7234fea1c6d4cf0c71596
Aidar Sadykov <aidarsadykov94@gmail.com>
2021-09-10 14:55:12 +0200

    Change EmergencyBeacon blueprint

--- src/constants/BLUEPRINTS.ts
+++ src/constants/BLUEPRINTS.ts
@@ -4,11 +4,10 @@ import { ITEMS } from "./ITEMS";
 export const BLUEPRINTS = [
   new Blueprint(0, "Starship", 100),
   new Blueprint(1, "Engineering Bay", 20),
-  new Blueprint(2, "Emergency Beacon", 0, [ITEMS[0], ITEMS[1]]),
-  //new Blueprint(2, "Emergency Beacon", 0, [
-  //  ITEMS[10],
-  //  ITEMS[8],
-  //  ITEMS[5],
-  //  ITEMS[2],
-  //]),
+  new Blueprint(2, "Emergency Beacon", 0, [
+    ITEMS[11],
+    ITEMS[8],
+    ITEMS[5],
+    ITEMS[2],
+  ]),
 ];
