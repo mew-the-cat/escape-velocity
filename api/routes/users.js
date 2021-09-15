a272390991c285a675527321d99c0d8625ab653b
Aleksandr Kudriavtcev <a.kudriavtcev@valtech.com>
2021-09-15 16:40:45 +0200

    refactor

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { StatusCodes } = require('http-status-codes');
 
 const userRoutes = express.Router();
 
@@ -6,17 +7,23 @@ const dbo = require("../db/conn");
 
 
 userRoutes.route("/users/:userId").post(function (req, res) {
-    let db_connect = dbo.getDb("game");
+    let db_connect = dbo.getDb("escapeVelocity");
+    const userId = req.params.userId;
     let userData = {
-        userId: req.params.userId,
+        userId: userId,
         characters: req.body.characters,
         tiles: req.body.tiles,
         phase: req.body.phase,
         constructions: req.body.constructions,
         userPrompt: req.body.userPrompt,
     };
-    db_connect.collection("users").insertOne(userData, function (err, res) {
-        if (err) throw err;
+
+    db_connect.collection("users").insertOne(userData, function (err) {
+        if (err) {
+            return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR)
+        } else {
+            return res.status(StatusCodes.OK).send("Created user with id " + userId)
+        }
     });
 });
 
